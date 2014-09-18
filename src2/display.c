8c16cb45884275621a033ab5d4b71f357036852e
Antoine Lamielle <antoine.lamielle@0x010c.fr>
2014-09-18 15:43:03 +0200

    di_updateBoxing

---
+++ src2/display.c
@@ -0,0 +1,89 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ncurses.h>
+#include <string.h>
+
+#include "display.h"
+
+void di_init()
+{
+	/*Initialisation de ncurses*/
+	initscr();
+	noecho();
+	keypad(stdscr,TRUE);
+	curs_set(0);
+	start_color();
+
+	/*Mise en place des paires de couleurs*/
+	init_pair(1, COLOR_WHITE, COLOR_CYAN); /*1*/
+	init_pair(2, COLOR_YELLOW, COLOR_BLACK); /*2*/
+	init_pair(3, COLOR_GREEN, COLOR_CYAN); /*3*/
+}
+
+void di_refresh()
+{
+	refresh();
+}
+
+void di_updatePlaylist(int firstIndex, int selectedIndex, int playedIndex)
+{
+	
+}
+
+void di_updateFilelist(int firstIndex, int selectedIndex)
+{
+	
+}
+
+void di_updateBoxing(char *title1, char *title2)
+{
+	/* Draw the top-line */
+	int i,j;
+	mvprintw(0,0,"+");
+	for(i=1;i<(SIZE_LEFT-strlen(title1))/2-1 && strlen(title1)+4 < SIZE_LEFT;i++)
+		mvprintw(0,i,"-");
+	mvprintw(0,i++,"|");
+	for(j=0;i<SIZE_LEFT-2 && j<strlen(title1);j++,i++)
+		mvprintw(0,i,"%c",title1[j]);
+	mvprintw(0,i++,"|");
+	for(;i<SIZE_LEFT-1;i++)
+		mvprintw(0,i,"-");
+	mvprintw(0,i++,"+");
+	
+	mvprintw(0,i++,"+");
+	for(;i<SIZE_LEFT+(SIZE_RIGHT-strlen(title2))/2-1 && strlen(title2)+4 < SIZE_RIGHT;i++)
+		mvprintw(0,i,"-");
+	mvprintw(0,i++,"|");
+	for(j=0;i<SIZE_LEFT+SIZE_RIGHT-2 && j<strlen(title2);j++,i++)
+		mvprintw(0,i,"%c",title2[j]);
+	mvprintw(0,i++,"|");
+	for(;i<SIZE_LEFT+SIZE_RIGHT-1;i++)
+		mvprintw(0,i,"-");
+		
+	mvprintw(0,i++,"+");
+	
+	/* Draw the vertical-lines */
+	for(j=1;j<HEIGHT;j++)
+	{
+		mvprintw(j,0,"|");
+		mvprintw(j,SIZE_LEFT-1,"|");
+		mvprintw(j,SIZE_LEFT,"|");
+		mvprintw(j,SIZE_LEFT+SIZE_RIGHT-1,"|");
+	}
+	
+	/* Draw the bottom-line */
+	mvprintw(HEIGHT-1,0,"+");
+	for(i=1;i<SIZE_LEFT-1;i++)
+		mvprintw(HEIGHT-1,i,"-");
+	mvprintw(HEIGHT-1,SIZE_LEFT-1,"+");
+	
+	mvprintw(HEIGHT-1,SIZE_LEFT,"+");
+	for(i=SIZE_LEFT+1;i<SIZE_LEFT+SIZE_RIGHT-1;i++)
+		mvprintw(HEIGHT-1,i,"-");
+	mvprintw(HEIGHT-1,SIZE_LEFT+SIZE_RIGHT-1,"+");
+}
+
+void di_end()
+{
+	endwin();
+}
