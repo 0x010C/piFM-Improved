09d3c68d667a1fb44a0d6f5dd8cea01e4deb1fe0
Antoine Lamielle <antoine.lamielle@0x010c.fr>
2014-10-11 22:03:37 +0200

    Création des fichiers launchpifm.c et .h, et mise en place de la structure flobale

---
+++ src2/launchpifm.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "launchpifm.h"
+
+void lp_loop()
+{
+
+}
+
+void lp_pause()
+{
+
+}
+
+void lp_ontinue()
+{
+
+}
+
+void lp_stop()
+{
+
+}
+
---
+++ src2/launchpifm.h
@@ -0,0 +1,20 @@
+#ifndef __LAUNCHPIFM_H
+#define __LAUNCHPIFM_H
+
+
+/*
+ * Includes
+ */
+#include "main.h"
+
+
+/*
+ * Functions' header
+ */
+void lp_loop();
+void lp_pause();
+void lp_ontinue();
+void lp_stop();
+
+
+#endif
